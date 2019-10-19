dc3728c4deecd929292c36eb881959c5a5096aed
Dmitry Svistun <dima1002010@live.com>
2019-10-20 00:49:46 +0300

    добавил propTypes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import WelcomeScreen from "../welcome-screen/Welcome-screen.jsx";
 
 export const App = () => {
-  return <WelcomeScreen />;
+  return <WelcomeScreen
+    gameTime={7}
+    possibleErrors={3}
+  />;
 };
 
 export default App;
